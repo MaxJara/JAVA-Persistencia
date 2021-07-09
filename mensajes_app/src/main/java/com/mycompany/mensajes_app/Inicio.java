cae0ea3f58be14af00e48aff8f4b3b7f1eabc11a
MaxJara <maxmata77@gmail.com>
2021-07-09 18:36:33 -0500

    Se uso constructores

--- mensajes_app/src/main/java/com/mycompany/mensajes_app/Inicio.java
+++ mensajes_app/src/main/java/com/mycompany/mensajes_app/Inicio.java
@@ -6,20 +6,50 @@
 package com.mycompany.mensajes_app;
 
 import java.sql.Connection;
+import java.util.Scanner;
 /**
  *
  * @author m.mata.jara
  */
 public class Inicio {
     
-    public static void main (String[] args){
-        
-        Conexion conexion = new Conexion();
-        
-        try(Connection cnx = conexion.get_connection()){
+    public static void main(String[] args){
+    
+       Scanner sc = new Scanner(System.in);
+       
+       int opcion=0;
+       
+       do{
+            System.out.println("-----------------");
+            System.out.println(" Aplicacion de mensajes");
+            System.out.println(" 1. crear mensaje");
+            System.out.println(" 2. listar mensajes");
+            System.out.println(" 3. editar mensaje");
+            System.out.println(" 4. eliminar mensaje");
+            System.out.println(" 5. salir");
+            //leemos la opcion del usuario
+            opcion = sc.nextInt();
             
-        }catch(Exception e){
-            System.out.println(e);
-        }
+            switch (opcion){
+                case 1:
+                    mensajesService.crearMensaje();
+                    break;
+                case 2:
+                    mensajesService.listarMensajes();
+                    break;
+                case 3:
+                    mensajesService.borrarMensaje();
+                    break;
+                case 4:
+                    mensajesService.editarMensaje();
+                    break;
+                default:
+                    break;
+            }
+           
+       }while(opcion != 5);
+        
+       
     }
+    
 }
