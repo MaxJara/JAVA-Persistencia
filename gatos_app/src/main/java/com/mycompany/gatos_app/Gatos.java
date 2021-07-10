664ddabfb7819a81edc2c7da2a7115be091ddc19
MaxJara <maxmata77@gmail.com>
2021-07-10 16:19:49 -0500

    Se realizo ultimo commit

---
+++ gatos_app/src/main/java/com/mycompany/gatos_app/Gatos.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gatos_app;
+
+/**
+ *
+ * @author m.mata.jara
+ */
+public class Gatos {
+    String id;
+    String url;
+    String apikey = "1b2f1430-7798-49de-a369-21dc8df854be";
+    String image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
+    
+}
